a63ace6ef648da2d185926fad444048f56cfb628
Scripted Scholar <129041827+ScriptedScholar@users.noreply.github.com>
2025-03-03 17:03:57 -0500

    3Mar25 1703 - Started Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -8,6 +8,8 @@ import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
 import swervelib.math.Matter;
 
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide
  * numerical or boolean constants. This
@@ -50,4 +52,88 @@ public final class Constants {
     public static final double RIGHT_X_DEADBAND = 0.1;
     public static final double TURN_CONSTANT = 6;
   }
+
+  public static class ControllerDriverConstants { // Logitech Gamepad F310 in X Mode
+    public static final int JOYSTICK_PORT = 0;
+
+    public static final int AXIS_LEFT_Y = 1;
+    public static final int AXIS_LEFT_X = 0;
+    public static final int AXIS_RIGHT_Y = 5;
+    public static final int AXIS_RIGHT_X = 4;
+
+    public static final int BUTTON_A = 1;
+    public static final int BUTTON_B = 2;
+    public static final int BUTTON_Y = 4;
+    public static final int BUTTON_X = 3;
+
+    public static final int BUTTON_BACK = 7;
+    public static final int BUTTON_START = 8;
+
+    public static final int BUTTON_BUMPER_RIGHT = 6;
+    public static final int BUTTON_BUMPER_LEFT = 5;
+
+    public static final double DEADBAND_AXIS_LEFT_Y = 0.05;
+    public static final double DEADBAND_AXIS_LEFT_X = 0.05;
+    public static final double DEADBAND_AXIS_RIGHT_Y = 0.05;
+    public static final double DEADBAND_AXIS_RIGHT_X = 0.05;
+  }
+
+  public static class ControllerOperatorConstants { // Logitech Gamepad F310 in D Mode
+    public static final int JOYSTICK_PORT = 1;
+
+    public static final int AXIS_LEFT_Y = 1;
+    public static final int AXIS_LEFT_X = 0;
+    public static final int AXIS_RIGHT_Y = 3;
+    public static final int AXIS_RIGHT_X = 2;
+
+    public static final int BUTTON_A = 2;
+    public static final int BUTTON_B = 3;
+    public static final int BUTTON_Y = 4;
+    public static final int BUTTON_X = 1;
+
+    public static final int BUTTON_BACK = 9;
+    public static final int BUTTON_START = 10;
+
+    public static final int BUTTON_BUMPER_TOP_RIGHT = 6;
+    public static final int BUTTON_BUMPER_TOP_LEFT = 5;
+    public static final int BUTTON_BUMPER_BOTTOM_RIGHT = 8;
+    public static final int BUTTON_BUMPER_BPTTOM_LEFT = 7;
+
+    public static final double DEADBAND_AXIS_LEFT_Y = 0.05;
+    public static final double DEADBAND_AXIS_LEFT_X = 0.05;
+    public static final double DEADBAND_AXIS_RIGHT_Y = 0.05;
+    public static final double DEADBAND_AXIS_RIGHT_X = 0.05;
+  }
+
+  public static class ElevatorConstants {
+    public static final int MOTOR_LEFT_CANID = 20;
+    public static final MotorType MOTOR_LEFT_TYPE = MotorType.kBrushless;
+    public static final boolean MOTOR_LEFT_IS_INVERTED = true;
+
+    public static final int MOTOR_RIGHT_CANID = 21;
+    public static final MotorType MOTOR_RIGHT_TYPE = MotorType.kBrushless;
+    public static final boolean MOTOR_RIGHT_IS_INVERTED = true;
+
+    public static final double SLOT_ZERO_P = 0.5;
+    public static final double SLOT_ZERO_I = 0.0;
+    public static final double SLOT_ZERO_D = 0.0;
+    public static final double SLOT_ZERO_F = 0.0;
+
+    public static final double HEIGHT_CORAL_LEVEL_FOUR = -52.2; // Estimated based on manual lift.
+    public static final double HEIGHT_CORAL_LEVEL_THREE = -28.00; // Almost worked at 25. Need a hair down.
+    public static final double HEIGHT_CORAL_LEVEL_TWO = -12.00; // Almost worked at 10. Need a hair down.
+    public static final double HEIGHT_CORAL_LEVEL_ONE = -5; // UNKOWN
+    public static final double HEIGHT_CORAL_INTAKE = -0.25; // UNKOWN
+    public static final double HEIGHT_STOWED = -0.5; // UNKOWN
+    public static final double HEIGHT_NO_POWER = 0.00;
+  }
+
+  public static class CoralConstants {
+
+  }
+
+  public static class FlapConstants {
+
+  }
+
 }
